5ef1f9b8c8887f48c001c9bdf75159d1afc42a54
Caleb Singleton <cmsingle12@gmail.com>
2025-09-26 16:52:28 -0400

    Added data normalization and implemented features into pipeline

--- python/psqlPipeline.py
+++ python/psqlPipeline.py
@@ -6,15 +6,33 @@ def pipeline():
     # Pull in csv
     filename = 'AAPL.csv'
     path = os.path.join('data', 'AAPL.csv')
-    print(path)
     df = pd.read_csv(path)
 
+    # The methods used to store the Yfinance data to CSV produce two stray rows at the top of the csv. This removes those rows.
+    df.drop([0,1],inplace=True)
+
+    # Check for and handle missing values
+    if df.isnull().sum().sum() != 0:
+        print("Missing Values Detected. Please try reloading the data. ")
+        exit(0)
+
+    # Ensure certain columns are numeric
+    df['Close'] = pd.to_numeric(df['Close'])
+    df['High'] = pd.to_numeric(df['High'])
+    df['Low'] = pd.to_numeric(df['Low'])
+    df['Open'] = pd.to_numeric(df['Open'])
+    df['Volume'] = pd.to_numeric(df['Volume'])
+
+    # Add new columns to the dataframe of popular financial indicators
     features.SMA(df, 10)
     features.EMA(df, 10)
     features.pctChange(df)
     features.volatility(df, 10)
 
-    print(df.head())
+    print(df["Close"].dtype)
+    print(df.tail())
+
+    
 
 pipeline()
     
